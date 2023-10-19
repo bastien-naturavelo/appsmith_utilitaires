7ddfc98e0573f2ede43656e71980b6279f8c7399
unknown <bastien@naturavelo.com>
2023-10-19 11:41:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+      // AMD
+    define([], factory);
+  } else if (typeof exports === 'object') {
+      // CommonJS
+    module.exports = factory();
+  } else {
+    // Browser globals (Note: root is window)
+    root.returnExports = factory();
+  }
+}(this, function () {
+  // Methods
+  function formatDate(date){
+		date = new Date(date);
+		return new Intl.DateTimeFormat('fr-FR', { dateStyle: 'full', timeStyle: 'long', timeZone: 'Europe/Paris' }).format(date);
+	};
+  function formatCurrency(amount){
+		return new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(amount);
+	};
+
+  // Exposed public methods
+  return {
+      formatDate: formatDate,
+      formatCurrency: formatCurrency
+  }
+}));
